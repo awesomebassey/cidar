25e0df8829eecf171e51ff49e08ab40d756a29f8
Awesome Bassey <basseyawesome@gmail.com>
2022-06-17 15:01:28 +0100

    Added Gilroy Font

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,10 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Header({ transparent }) {
     const images = process.env.PUBLIC_URL + "/assets/images"
     let initialImage;
-    let initialClasses;
     if (transparent) {
         initialImage = images + "/logos/type-black.png"
-        initialClasses = "nav-link fw-bold"
     } else {
         initialImage = images + "/logos/type-white.png"
-        initialClasses = "nav-link fw-bold text-white"
     }
     const $ = window.$
     $(function () {
@@ -17,11 +14,9 @@ export default function Header({ transparent }) {
             if ($(window).scrollTop() > 10) {
                 $('.navbar').addClass('active');
                 $('.navbar img').attr('src', images + "/logos/type-black.png");
-                $('.nav-link').each(function () { $(this).removeClass('text-white') })
             } else {
                 $('.navbar').removeClass('active');
                 $('.navbar img').attr('src', initialImage);
-                $('.nav-link').each(function (e) { $(e).addClass(initialClasses) })
             }
         });
     });
@@ -39,13 +34,13 @@ export default function Header({ transparent }) {
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className={initialClasses} to={'/projects'}>Projects</NavLink>
+                                <NavLink className={`nav-link fw-bold ${!transparent && 'transparent'}`} to={'/projects'}>Projects</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className={initialClasses} to={'/about'}>About Us</NavLink>
+                                <NavLink className={`nav-link fw-bold ${!transparent && 'transparent'}`} to={'/about'}>About Us</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className={initialClasses} to={'/start'}>Start a Project</NavLink>
+                                <NavLink className={`nav-link fw-bold ${!transparent && 'transparent'}`} to={'/start'}>Start a Project</NavLink>
                             </li>
                         </ul>
                         <Link className="btn btn-success ms-lg-3 nav-btn py-2 px-4" to={'/contact'}>Reach Us</Link>
